2441db4535c49748333754ce849c28b8d17aa82c
Ivan <ivan@MacBook-Pro-Ivan.local>
2023-02-03 18:45:58 +0300

    Add compiler without variables and labels

--- compiler.py
+++ compiler.py
@@ -1,7 +1,7 @@
-import re
 from hack_specifications import destination_specification
 from hack_specifications import computation_specification
 from hack_specifications import jump_specification
+
 reg_table = {}
 
 
@@ -20,28 +20,18 @@ class FileParser:
     def read_line_from_file(self):
         with open(self.filename, "r") as file:
             for line in file:
-                yield line.strip('\n')
+                yield line.strip("\n")
 
     def parse_command(self, file_line):
-        if "@" in line:
-            pass
+        if "@" in file_line:
+            command_type = "a-command"
         else:
-            parsed_c_string = re.split(
-                "=|;", file_line.replace(" ", "")
-            )  # dest = comp ; jump
-            dest = parsed_c_string[0]
-            comp = parsed_c_string[1]
-            try:
-                jump = parsed_c_string[2]
-            except IndexError:
-                jump = "null"
             command_type = "c-command"
-            c_string = f"{dest};{comp};{jump}"
-            return command_type, c_string
+
+        return command_type, file_line
 
 
 class Compiler:
-
     def compile_string_to_machine_code(self, command_type, hack_string):
         match command_type:
             case "c-command":
@@ -53,27 +43,33 @@ class Compiler:
         return machine_code
 
     def _compile_c_string(self, c_string):
-
-        dest_in_binary = destination_specification[c_string.split(";")[0]]
-        comp_in_binary = computation_specification[c_string.split(";")[1]]
-        try:
-            jump_in_binary = jump_specification[c_string.split(";")[2]]
-        except KeyError:
-            jump_in_binary = "000"
-
-        return f"111{comp_in_binary}{dest_in_binary}{jump_in_binary}"
+        if "=" in c_string and ";" in c_string:
+            dest = destination_specification[c_string.split("=")[0]]
+            comp = computation_specification[c_string.split("=")[1]]
+            jump = jump_specification[c_string.split(";")[1]]
+        elif "=" in c_string:
+            dest = destination_specification[c_string.split("=")[0]]
+            comp = computation_specification[c_string.split("=")[1]]
+            jump = jump_specification["null"]
+        elif ";" in c_string:
+            dest = destination_specification["null"]
+            comp = computation_specification[c_string.split(";")[0]]
+            jump = jump_specification[c_string.split(";")[1]]
+        return f"111{comp}{dest}{jump}"
 
     def _compile_a_string(self, hack_string):
-        return hack_string
+        if hack_string[1:].isdigit():
+            return f"0{int(hack_string[1:]):015b}"
 
 
 if __name__ == "__main__":
     file_parser = FileParser("test.asm")
-    file_parser.delete_whitespaces_in_file()
     compiler = Compiler()
     for line in file_parser.read_line_from_file():
         command_type, hack_string = file_parser.parse_command(line)
         machine_code = compiler.compile_string_to_machine_code(
             command_type, hack_string
         )
-        print(machine_code)
+        # add into file "test.hack"
+        with open("test.hack", "a") as file:
+            file.write(machine_code + "\n")
